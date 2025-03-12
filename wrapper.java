3f4bd29b7f769a15b3a0ebd28c7a76276d609d6e
NikhitaVR <nikhitavr.ai23@bmsce.ac.in>
2025-03-12 21:06:11 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class wrapper{
+  public static void main(String args[]){
+      Scanner in = new Scanner(System.in);
+      int a = 10; 
+      Integer boxedint = a;
+      int unboxedint = boxedint;
+      System.out.println("Boxed Integer : "+boxedint);
+      System.out.println("UnBoxed Integer : "+unboxedint);
+      float f = 10.0f; 
+      Float boxedfloat = f;
+      float unboxedfloat = boxedfloat;
+      System.out.println("Boxed Float : "+boxedfloat);
+      System.out.println("UnBoxed float : "+unboxedfloat);
+      char c = 'N';
+      Character boxedchar = c;
+      char unboxedchar = boxedchar;
+      System.out.println("Boxed Character : "+boxedchar);
+      System.out.println("UnBoxed Character : "+unboxedchar);
+      short s = 20; 
+      Short boxedshort = s;
+      short unboxedshort = boxedshort;
+      System.out.println("Boxed Short : "+boxedshort);
+      System.out.println("UnBoxed short : "+unboxedshort);
+      long l = 10000L; 
+      Long boxedlong = l;
+      long unboxedlong = boxedlong;
+      System.out.println("Boxed Long : "+boxedlong);
+      System.out.println("UnBoxed Long : "+unboxedlong);
+      double d  = 9.9; 
+      Double boxeddouble = d;
+      double unboxeddouble = boxeddouble;
+      System.out.println("Boxed Double : "+boxeddouble);
+      System.out.println("UnBoxed Diuble : "+unboxeddouble);
+      boolean b = true;
+      Boolean boxedbool = b;
+      boolean unboxedbool = boxedbool;
+      System.out.println("Boxed Boolean : "+boxedbool);
+      System.out.println("UnBoxed Boolean : "+unboxedbool);
+      byte m = 50; 
+      Byte boxedbyte = m;
+      byte unboxedbyte = boxedbyte;
+      System.out.println("Boxed Byte : "+boxedbyte);
+      System.out.println("UnBoxed Byte: "+unboxedbyte);
+
+
+
+     
+    }
+}
