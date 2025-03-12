a6d6617f4de0a0f6f8b2ae964a7d44daefbf2bf9
NikhitaVR <nikhitavr.ai23@bmsce.ac.in>
2025-03-12 16:02:10 +0530

    Add files via upload

---
+++ CHESS.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CHESS {
+    public static void main(String[] args) {
+        int size=8;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if((i+j)%2==0){
+                    System.out.print("**");
+
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.print("\n");
+        }
+    }
+}
